ca1fb4066dcaf707a797500c5581363a4fe098cd
Leo Belyakov <lbelyakov@cochlear.com>
2025-04-09 01:30:40 +1000

    Added themes and resources

---
+++ app/src/main/java/com/leoapps/test/Colors.kt
@@ -0,0 +1,10 @@
+package com.leoapps.test
+
+import androidx.compose.ui.graphics.Color
+
+val appYellow = Color(0xFFFFDE39)
+val appGreen = Color(0xFF3EB075)
+val appRed = Color(0xFFC52943)
+val placeholder = Color(0xFFF2F2F7)
+val primaryText = Color(0xFF393746)
+val secondaryText = Color(0xFF898A8D)
